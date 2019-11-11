588509b6838054a5837baee68e4c8e965bbbda29
Jesse <jetpaine@gmail.com>
2019-11-11 17:07:46 -0500

    full code added

--- documents/netbeansprojects/conwaygol/src/main/java/conwaygol/ConwayGoL.java
+++ documents/netbeansprojects/conwaygol/src/main/java/conwaygol/ConwayGoL.java
@@ -1,20 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Dox
- */
-public class ConwayGoL {
+package connectfour;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import java.awt.event.*;
+//adding in some text to save for git
 
-    /**
-     * @param args the command line arguments
-     */
+public class ConnectFour {
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(new Runnable () {
+            public void run() {
+                initializeGUI();
+            }
+        });
     }
-    
+    public static void initializeGUI() {
+        JFrame f = new JFrame();
+        MyPanel p = new MyPanel();
+        p.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                System.out.println("pressed");
+                p.repaint();
+            }
+        });
+        p.addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                System.out.println("dragged");
+            }
+        });
+        f.setSize(900, 600);
+        f.add(p);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 }
+
+
